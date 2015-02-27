9d921b257a445724d33df1b9d389425715c4adc7
Ignacio Galindo <joiggama@gmail.com>
2015-02-26 18:22:40 -0600

    Add New with tests

--- money.go
+++ money.go
@@ -1,12 +1,35 @@
 package money
 
+import (
+	"fmt"
+)
+
 type Options map[string]interface{}
+
+func addSymbol(result string, currency map[string]interface{}, options Options) string {
+	var space string
+
+	if options["with_symbol_space"].(bool) {
+		space = " "
+	}
+
+	if currency["symbol_first"].(bool) {
+		result = fmt.Sprintf("%s%s%s", currency["symbol"], space, result)
+	} else {
+		result = fmt.Sprintf("%s%s%s", result, space, currency["symbol"], space)
+	}
+
+	return result
+}
 
 func defaults() Options {
 	return Options{
-		"currency":      "usd",
-		"no_cents":      false,
-		"with_currency": false,
+		"currency":                 "usd",
+		"with_cents":               true,
+		"with_currency":            false,
+		"with_symbol":              true,
+		"with_symbol_space":        false,
+		"with_thousands_separator": true,
 	}
 }
 
@@ -18,4 +41,36 @@ func override(original, override Options) Options {
 	}
 
 	return original
+}
+
+func New(val float64, opts ...Options) (result string) {
+	options := defaults()
+
+	if len(opts) > 0 {
+		options = override(options, opts[0])
+	}
+
+	currency := currencies[options["currency"].(string)]
+
+	integer, fractional := splitValue(val)
+
+	if options["with_thousands_separator"].(bool) {
+		result = separateThousands(integer, currency["thousands_separator"].(string))
+	} else {
+		result = integer
+	}
+
+	if options["with_cents"].(bool) && currency["subunit"] != nil {
+		result = fmt.Sprintf("%s%s%s", result, currency["decimal_mark"].(string), fractional)
+	}
+
+	if options["with_symbol"].(bool) {
+		result = addSymbol(result, currency, options)
+	}
+
+	if options["with_currency"].(bool) {
+		result = fmt.Sprintf("%s %s", result, currency["iso_code"])
+	}
+
+	return result
 }
--- money_test.go
+++ money_test.go
@@ -11,12 +11,93 @@ func TestDefaults(t *testing.T) {
 		t.Error("Expected default currency to be usd")
 	}
 
-	if defaults["no_cents"].(bool) != false {
-		t.Error("Expected default no_cents to be false")
+	if defaults["with_cents"].(bool) != true {
+		t.Error("Expected default with_cents to be true")
 	}
 
 	if defaults["with_currency"].(bool) != false {
 		t.Error("Expected default with_currency to be false")
+	}
+}
+
+func TestNewDefaults(t *testing.T) {
+	currency := New(10)
+	expected := "$10.00"
+
+	if currency != expected {
+		t.Errorf("Expected %s but got %s", expected, currency)
+	}
+}
+
+func TestNewWithCurrency(t *testing.T) {
+	currency := New(10, Options{"with_currency": true})
+	expected := "$10.00 USD"
+
+	if currency != expected {
+		t.Errorf("Expected %s but got %s", expected, currency)
+	}
+}
+
+func TestNewWithCurrencyWhenCanadian(t *testing.T) {
+	currency := New(10, Options{"currency": "cad", "with_currency": true})
+	expected := "$10.00 CAD"
+
+	if currency != expected {
+		t.Errorf("Expected %s but got %s", expected, currency)
+	}
+}
+
+func TestNewWhenJapanese(t *testing.T) {
+	currency := New(10, Options{"currency": "jpy"})
+	expected := "¥10"
+
+	if currency != expected {
+		t.Errorf("Expected %s but got %s", expected, currency)
+	}
+}
+
+func TestNewWhenJapaneseOverThousand(t *testing.T) {
+	currency := New(1000, Options{"currency": "jpy"})
+	expected := "¥1,000"
+
+	if currency != expected {
+		t.Errorf("Expected %s but got %s", expected, currency)
+	}
+}
+
+func TestNewWhenJapaneseOverTenThousand(t *testing.T) {
+	currency := New(10000, Options{"currency": "jpy"})
+	expected := "¥10,000"
+
+	if currency != expected {
+		t.Errorf("Expected %s but got %s", expected, currency)
+	}
+}
+
+func TestNewWhenJapaneseOverHundredThousand(t *testing.T) {
+	currency := New(100000, Options{"currency": "jpy"})
+	expected := "¥100,000"
+
+	if currency != expected {
+		t.Errorf("Expected %s but got %s", expected, currency)
+	}
+}
+
+func TestNewWhenJapaneseOverMillion(t *testing.T) {
+	currency := New(1000000, Options{"currency": "jpy"})
+	expected := "¥1,000,000"
+
+	if currency != expected {
+		t.Errorf("Expected %s but got %s", expected, currency)
+	}
+}
+
+func TestNewWithoutCents(t *testing.T) {
+	currency := New(10, Options{"with_cents": false})
+	expected := "$10"
+
+	if currency != expected {
+		t.Errorf("Expected %s but got %s", expected, currency)
 	}
 }
 

f459c07f75795fe2e9cc2ab674d5ac543f4c0ff1
Ignacio Galindo <joiggama@gmail.com>
2015-02-26 18:19:06 -0600

    Change thousandsSeparator value argument, now is string

--- helpers.go
+++ helpers.go
@@ -2,30 +2,28 @@ package money
 
 import (
 	"math"
-	"strconv"
 	"strings"
 )
 
-func separateThousands(value int64, separator string) string {
-	v := strconv.FormatInt(value, 10)
-	s := len(v) / 3
-	m := int(math.Mod(float64(len(v)), 3))
+func separateThousands(value, separator string) string {
+	s := len(value) / 3
+	m := int(math.Mod(float64(len(value)), 3))
 
 	if m > 0 {
 		s++
 	}
 
 	if s == 0 {
-		return v
+		return value
 	}
 
 	r := make([]string, s)
 
 	for i := 0; i < len(r); i++ {
 		if i == 0 && m > 0 {
-			r[i] = v[i : i+m]
+			r[i] = value[i : i+m]
 		} else {
-			r[i] = v[i : i+3]
+			r[i] = value[i : i+3]
 		}
 	}
 
